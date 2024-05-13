74c841f07b5c00d100379bd5e055615b24207553
Libanad <105227011+Libanad@users.noreply.github.com>
2024-05-13 21:14:42 +0545

    createpost page created and make it workable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import PostDetails from './pages/PostDetails'
+import CreatePost from './pages/CreatePost'
 
 
 const App = () => {
@@ -13,7 +14,9 @@ const App = () => {
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/login" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
+      <Route exact path="/write" element={<CreatePost/>}/>
       <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
+      
 
 
      </Routes>
