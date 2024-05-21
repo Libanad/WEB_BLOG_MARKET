84742393fba6d73dd971c39dd747607db703f611
Libanad <105227011+Libanad@users.noreply.github.com>
2024-05-21 21:37:50 +0545

    commit done

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+
+
+const Menu = () => {
+  return (
+    <div className="bg-black w-[200px] flex flex-col items-start absolute top-8">
+      <h3 className="text-white text-lg hover:text-gray-500">Login</h3>
+      <h3 className="text-white text-lg hover:text-gray-500">Register</h3>
+    </div>
+  )
+}
+
+export default Menu
