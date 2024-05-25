7b69140bbf7ea1c57fa15989f7e5c7e42965e045
Libanad <105227011+Libanad@users.noreply.github.com>
2024-05-25 23:12:29 +0545

    register validation done

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const app=express()
 const dotenv=require('dotenv')
-
+const cors=require('cors')
 const authRoute=require('./routes/auth')
 const userRoute=require('./routes/users')
 const postRoute=require('./routes/posts')
@@ -24,11 +24,13 @@ const connectDB=async()=>{
 // middlewares
 dotenv.config()
 app.use(express.json())
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
 app.use("/api/comments",commentsRoute)
+
 
 app.listen(process.env.PORT,()=>{
     connectDB()
