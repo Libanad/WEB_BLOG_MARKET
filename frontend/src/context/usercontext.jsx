ddf44b9238c7788f8cee17377190833ee90fe495
Libanad <105227011+Libanad@users.noreply.github.com>
2024-05-29 21:24:06 +0545

    Making the user logged in all the time

--- frontend/src/context/usercontext.jsx
+++ frontend/src/context/usercontext.jsx
@@ -6,6 +6,16 @@ export const UserContext = createContext();
 // Create a provider component
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null); // Initialize state to manage user
+  const getUser=async()=>{
+    try {
+        const res=await axios.get(URL+"/api/auth/refetch",{withCredentials:true})
+        console.log(res.data)
+        
+    } catch (error) {
+        console.log(err)
+        
+    }
+  }
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
