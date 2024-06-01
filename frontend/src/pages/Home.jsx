35aa67aeae540207740978cbe17434e120b3b508
Libanad <105227011+Libanad@users.noreply.github.com>
2024-06-01 20:31:42 +0545

    post fetched

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import Footer from "../components/Footer"
 import HomePost from "../components/HomePosts"
 import Navbar from "../components/Navbar"
+import { useEffect } from "react"
 
 
 const Home = () => {
@@ -10,12 +11,15 @@ const Home = () => {
     try {
       const res= await axios.get(URL+"/api/posts/")
       console.log(res.data)
-    } catch (error) {
-        console.log(error)
+    } catch (err) {
+        console.log(err)
     }
     
   }
-  
+  useEffect(()=>{
+    fetchPost()
+
+  },[])
 
 
   return (
