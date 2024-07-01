ccca94ff142d175992b9b7729ad838e934275aec
Libanad <105227011+Libanad@users.noreply.github.com>
2024-07-01 19:39:48 +0545

    asdfghj

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -2,11 +2,11 @@ import axios from "axios"
 import { useContext, useEffect, useState } from "react"
 import { BiEdit } from 'react-icons/bi'
 import { MdDelete } from 'react-icons/md'
-import { useLocation, useParams } from "react-router-dom"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
 import Comment from "../components/Comment"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
-import { URL,IF } from "../url"
+import { URL, IF } from "../url"
 import { UserContext } from "../context/usercontext"
 import Loader from "../components/Loader"
 
@@ -15,15 +15,16 @@ const PostDetails = () => {
 
   const postId = useParams().id
   const [post, setPost] = useState({})
-  const {user}=useContext(UserContext)
-  const [loader, setLoader]=useState(false)
-  
+  const { user } = useContext(UserContext)
+  const [loader, setLoader] = useState(false)
+  const navigate = useNavigate()
+
 
 
   const fetchPost = async () => {
     setLoader(true)
     try {
-      const res = await axios.get(URL+"/api/posts/"+postId)
+      const res = await axios.get(URL + "/api/posts/" + postId)
       // console.log(res.data)
       setPost(res.data)
       setLoader(false)
@@ -34,6 +35,18 @@ const PostDetails = () => {
     }
   }
 
+  const handleDeletePost = async () => {
+    try {
+      const res = await axios.delete(URL + "/api/posts/" + postId,{withCredentials:true})
+      console.log(res.data)
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+
+    }
+
+  }
+
   useEffect(() => {
     fetchPost()
 
@@ -41,13 +54,13 @@ const PostDetails = () => {
   return (
     <div>
       <Navbar />
-     {loader?<div className="h-[80vh] flex justify-center items-center w-full"><Loader/></div> :<div className="px-8 px-[200px] mt-8">
+      {loader ? <div className="h-[80vh] flex justify-center items-center w-full"><Loader /></div> : <div className="px-8 px-[200px] mt-8">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold text-black md:text-3xl">{post.title}</h1>
-          {user?._id===post?.userId && <div className="flex items-center justify-end space-x-2">
-    
+          {user?._id === post?.userId && <div className="flex items-center justify-end space-x-2">
+
             <p className="mr-2"><BiEdit /></p>
-            <p><MdDelete /></p>
+            <p className="cursor-pointer" onClick={handleDeletePost}><MdDelete /></p>
           </div>}
         </div>
         <div className="flex items-center justify-between mt-2 md:mt-4">
@@ -57,20 +70,20 @@ const PostDetails = () => {
             <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
           </div>
         </div>
-        <img src={IF+post.photo} className="w-3/4 mx-auto" alt="" />
+        <img src={IF + post.photo} className="w-3/4 mx-auto" alt="" />
         <p className="mx-auto mt-8">
           {post.desc}
         </p>
         <div className="flex items-center mt-8 space-x-4 font-semibold">
           <p>categories:</p>
           <div className="flex justify-center items-center space-x-2">
-            {post.categories?.map((c,i)=>(
+            {post.categories?.map((c, i) => (
               <>
-              <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">{c}</div>
+                <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">{c}</div>
               </>
             ))}
-            
-            
+
+
           </div>
         </div>
         <div className="flex flex-col mt-4">
